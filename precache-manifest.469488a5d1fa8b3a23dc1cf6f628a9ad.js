d5954d6a617117c3d3e95e2016ba66122a0548b0
Musfiq420 <mrifat46@gmail.com>
2020-07-05 20:58:28 +0600

    Updates

---
+++ precache-manifest.469488a5d1fa8b3a23dc1cf6f628a9ad.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80d6e378efa84fffe6735ac0648a3e3",
+    "url": "/app-landing-page/index.html"
+  },
+  {
+    "revision": "1459aabee2dbf4318508",
+    "url": "/app-landing-page/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2cd902e503e54ee38918",
+    "url": "/app-landing-page/static/js/2.d0e5c2e4.chunk.js"
+  },
+  {
+    "revision": "277f9ac65ea629533c781a2e9dd27b69",
+    "url": "/app-landing-page/static/js/2.d0e5c2e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1459aabee2dbf4318508",
+    "url": "/app-landing-page/static/js/main.448de39c.chunk.js"
+  },
+  {
+    "revision": "f53d64f61e08765ff61e",
+    "url": "/app-landing-page/static/js/runtime-main.42fba44e.js"
+  },
+  {
+    "revision": "80b27f7bc1633b6577b766e916d7ef78",
+    "url": "/app-landing-page/static/media/background.80b27f7b.svg"
+  },
+  {
+    "revision": "c3bd8ce10bc3dae26c876f7f0bb21dcd",
+    "url": "/app-landing-page/static/media/computer.c3bd8ce1.svg"
+  },
+  {
+    "revision": "0b961e4c44b9478f82f1bd4ed0d4f5e0",
+    "url": "/app-landing-page/static/media/facebook.0b961e4c.svg"
+  },
+  {
+    "revision": "71a15cbd76dd4379d88938186d520f8d",
+    "url": "/app-landing-page/static/media/healthy.71a15cbd.svg"
+  },
+  {
+    "revision": "12cd36dd4cc6c10e6a952bf8e3379af7",
+    "url": "/app-landing-page/static/media/logo.12cd36dd.svg"
+  },
+  {
+    "revision": "cba242b821a3ca3096df95a9ed512077",
+    "url": "/app-landing-page/static/media/medicine.cba242b8.svg"
+  },
+  {
+    "revision": "04c2097ebdb4c5f826e387ad8670bc3e",
+    "url": "/app-landing-page/static/media/phone.04c2097e.svg"
+  },
+  {
+    "revision": "91e80acf9597605a8f24ade1d1042384",
+    "url": "/app-landing-page/static/media/question.91e80acf.svg"
+  },
+  {
+    "revision": "b6eab67df4f6c2af80aad8aeca8d467d",
+    "url": "/app-landing-page/static/media/twitter.b6eab67d.svg"
+  },
+  {
+    "revision": "a86b49cc66f7363b71d4cbe0bf93a621",
+    "url": "/app-landing-page/static/media/youtube.a86b49cc.svg"
+  }
+]);
